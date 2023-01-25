8a17024de64d578c44fb295306d5cdb64c80f89b
joaooviedo <joaovitorgiroldi@gmail.com>
2023-01-24 23:44:27 -0300

    Feat: Menu implementation

--- project-mod-6/src/pages/Home/index.tsx
+++ project-mod-6/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import * as S from "./style";
 import { ReactComponent as Search } from "../../assets/icons/search.svg";
+import Menu from "components/Menu";
 
 const Home = () => {
   return (
     <S.Home>
+      <Menu />
       <S.HomeContent>
         <header>
           <S.HomeHeaderDetails>
