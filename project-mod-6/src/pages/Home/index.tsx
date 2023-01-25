84935111a13c0dc00d09e333d5b54020c7934bf9
joaooviedo <joaovitorgiroldi@gmail.com>
2023-01-24 22:58:58 -0300

    Feat: implementing search design

--- project-mod-6/src/pages/Home/index.tsx
+++ project-mod-6/src/pages/Home/index.tsx
@@ -1,7 +1,27 @@
 import * as S from "./style";
+import { ReactComponent as Search } from "../../assets/icons/search.svg";
 
 const Home = () => {
-  return <S.Home></S.Home>;
+  return (
+    <S.Home>
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+      </S.HomeContent>
+    </S.Home>
+  );
 };
 
 export default Home;
--- project-mod-6/src/pages/Home/style.ts
+++ project-mod-6/src/pages/Home/style.ts
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components";
 
+
 export const Home = styled.section`
   ${({ theme }) => css`
     background-color: ${theme.colors.baseBg1};
@@ -8,5 +9,54 @@ export const Home = styled.section`
     color: ${theme.colors.textColor};
     display: flex;
     justify-content: space-between;
+  `}
+`;
+
+export const HomeContent = styled.main`
+  ${() => css`
+    width: calc(100% - 450px);
+    padding: 20px;
+    overflow: auto;
+    height: 100vh;
+    box-sizing: border-box;
+  `}
+`;
+
+export const HomeHeaderDetails = styled.div`
+  ${() => css`
+    display: flex;
+    justify-content: space-between;
+  `}
+`;
+
+export const HomeHeaderDetailsLogo = styled.h1`
+  ${({ theme }) => css`
+    ${theme.mixins.logoStyle};
+    text-transform: uppercase;
+    margin: 0;
+  `}
+`;
+
+export const HomeHeaderDetailsDate = styled.p`
+  ${() => css`
+    text-transform: capitalize;
+  `}
+`;
+
+export const HomeHeaderDetailsSearch = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    ${theme.mixins.input()};
+    padding-top: 0;
+    padding-bottom: 0;
+    padding-right: 0;
+    input {
+      background: transparent;
+      border: none;
+      height: 100%;
+      color: ${theme.colors.textColor};
+    }
   `}
 `;
