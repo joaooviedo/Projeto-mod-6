00af25d54d5703b401f947b107381a4d4ab0536d
joaooviedo <joaovitorgiroldi@gmail.com>
2023-02-12 20:43:09 -0300

    Feat: Order Item List

--- project-mod-6/src/components/ButtonToggle/index.tsx
+++ project-mod-6/src/components/ButtonToggle/index.tsx
@@ -1,6 +1,23 @@
 import * as S from "./style";
-const ButtonToggle = () => {
-    return()
+import React, { ButtonHTMLAttributes } from "react";
+
+type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type ButtonToggleProps = {
+  value: string;
+  active?: boolean;
+} & ButtonType;
+
+const ButtonToggle = ({
+  value,
+  active = false,
+  ...props
+}: ButtonToggleProps) => {
+  return (
+    <S.ButtonToggle {...props} active={active}>
+      {value}
+    </S.ButtonToggle>
+  );
 };
 
-export default ButtonToggle
+export default ButtonToggle;
