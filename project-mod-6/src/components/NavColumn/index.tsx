ba5d0767f460ad27f96e4da699a05a63c7e14a01
joaooviedo <joaovitorgiroldi@gmail.com>
2023-02-12 22:32:49 -0300

    Feat: Settings e Manage Products Back

--- project-mod-6/src/components/NavColumn/index.tsx
+++ project-mod-6/src/components/NavColumn/index.tsx
@@ -1,11 +1,18 @@
-
 import { ReactComponent as Market } from "assets/icons/market.svg";
 import { ReactComponent as Info } from "assets/icons/info.svg";
 import { ReactComponent as Users } from "assets/icons/users.svg";
 import * as S from "./style";
 import { RoutePath } from "types/routes";
+import NavColumnItem from "components/NavColumnItem";
+import { HTMLAttributes } from "react";
 
-const NavColumn = () => {
+type NavColumnType = HTMLAttributes<HTMLDivElement>;
+
+type NavColumnProps = {
+  activeRoute: RoutePath;
+} & NavColumnType;
+
+const NavColumn = ({activeRoute}: NavColumnProps) => {
   const items = [
     {
       icon: <Market />,
@@ -26,7 +33,20 @@ const NavColumn = () => {
       navigation: RoutePath.SETTINGS_USERS,
     },
   ];
-  return <S.NavColumn>{"Componente NavColumnItem"}</S.NavColumn>;
+  return (
+    <S.NavColumn>
+      {" "}
+      {items.map((item, key) => (
+        <NavColumnItem
+          active={item.navigation === activeRoute}
+          icon={item.icon}
+          title={item.title}
+          key={key}
+          subtitle={item.subtitle}
+        />
+      ))}
+    </S.NavColumn>
+  );
 };
 
 export default NavColumn;
--- project-mod-6/src/router.tsx
+++ project-mod-6/src/router.tsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Settings from "pages/Settings/index";
+import Home from "pages/Home/index";
 import { RoutePath } from "types/routes";
 import Login from "pages/Login";
-// import Settings from "pages/Settings/index";
+import ManageProducts from "components/ManageProducts";
+import Settings from "pages/Settings/index";
 
 const Router = () => {
   return (
     <Routes>
       <Route path={RoutePath.LOGIN} element={<Login />} />
-      <Route path={RoutePath.HOME} element={<Settings />} />
+      <Route path={RoutePath.HOME} element={<Home />} />
       <Route path={RoutePath.SETTINGS} element={<Settings />} />
+      <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
+
     </Routes>
   );
 };
