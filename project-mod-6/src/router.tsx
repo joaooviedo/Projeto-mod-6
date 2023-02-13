f76de923332816573e70592aafbe0a040b036bc7
joaooviedo <joaovitorgiroldi@gmail.com>
2023-02-12 22:11:40 -0300

    Feat: Nav Column Settings

--- project-mod-6/src/router.tsx
+++ project-mod-6/src/router.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import { RoutePath } from "./types/routes";
+import Settings from "pages/Settings/index";
+import { RoutePath } from "types/routes";
 import Login from "pages/Login";
-import Settings from "pages/settings";
+// import Settings from "pages/Settings/index";
 
 const Router = () => {
   return (
     <Routes>
       <Route path={RoutePath.LOGIN} element={<Login />} />
-      <Route path={RoutePath.HOME} element={<Home />} />
+      <Route path={RoutePath.HOME} element={<Settings />} />
       <Route path={RoutePath.SETTINGS} element={<Settings />} />
     </Routes>
   );
